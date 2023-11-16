cf2c8d871fb24a0bf78ebc80841e8e61eaa74ed4
patr543a <113888937+patr543a@users.noreply.github.com>
2023-11-16 11:51:07 +0100

    Added DbContext

---
+++ TurboCart.Infrastructure.Persistance.Contexts/TurboCartContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TurboCart.Domain.Entities;
+
+namespace TurboCart.Infrastructure.Persistance.Contexts;
+
+public class TurboCartContext
+    : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(localdb\\mssqllocaldb;Database=TurboCartDb;Trusted_Connection=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Customer>()
+                .HasMany(c => c.Bookings)
+                    .WithOne(b => b.Customer)
+                        .HasForeignKey(b => b.CustomerId)
+                        .HasPrincipalKey(c => c.CustomerId);
+    }
+}
