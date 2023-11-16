01b3b23eacbe60243a2414d10081601ede4178fe
patr543a <113888937+patr543a@users.noreply.github.com>
2023-11-16 13:54:28 +0100

    Added RepositoryBase

---
+++ TurboCart.Infrastructure.Persistance.Repositories/RepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TurboCart.Infrastructure.Persistence.Interfaces;
+
+namespace TurboCart.Infrastructure.Persistance.Repositories;
+
+internal class RepositoryBase<T>(DbContext _dbContext)
+    : IRepository<T>
+    where T : class
+{
+    private DbSet<T> _set = _dbContext.Set<T>();
+
+    public IEnumerable<T> GetAll()
+        => _set.ToArray();
+
+    public T? GetById(object entityId)
+        => _set.Find(entityId);
+
+    public void Save(T entity)
+        => _set.Add(entity);
+}
