f4776a65be21bada89bedaec29671f0a415837db
patr543a <113888937+patr543a@users.noreply.github.com>
2023-11-16 13:54:37 +0100

    Added repository interfaces

---
+++ TurboCart.Infrastructure.Persistence.Interfaces/Repositories/IBookingRepository.cs
@@ -0,0 +1,8 @@
+﻿using TurboCart.Domain.Entities;
+
+namespace TurboCart.Infrastructure.Persistence.Interfaces.Repositories;
+
+public interface IBookingRepository
+    : IRepository<Booking>
+{
+}
---
+++ TurboCart.Infrastructure.Persistence.Interfaces/Repositories/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using TurboCart.Domain.Entities;
+
+namespace TurboCart.Infrastructure.Persistence.Interfaces.Repositories;
+
+public interface ICustomerRepository
+    : IRepository<Customer>
+{
+}
