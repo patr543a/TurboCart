242d3e69af8a44ac772e31e44a19041beebca9f5
patr543a <113888937+patr543a@users.noreply.github.com>
2023-11-20 11:30:28 +0100

    Added TurboCartDK

---
+++ TurboCart.Presentation.Websites.TurboCartDK/Pages/Booking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TurboCart.Presentation.Websites.TurboCartDK.Pages
+{
+    public class BookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
