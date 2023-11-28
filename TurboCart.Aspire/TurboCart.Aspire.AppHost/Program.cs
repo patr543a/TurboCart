cdf44d7e1a9bd5398609e50e7f0877ceab858624
patr543a <113888937+patr543a@users.noreply.github.com>
2023-11-28 09:24:46 +0100

    Added more projects to Aspire

--- TurboCart.Aspire/TurboCart.Aspire.AppHost/Program.cs
+++ TurboCart.Aspire/TurboCart.Aspire.AppHost/Program.cs
@@ -1,7 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 //builder.AddProject<Projects.TurboCart_Tests_UoWRazorTestApp>("turbocart.tests.uowrazortestapp");
+//builder.AddProject<Projects.TurboCart_Tests_Api>("turbocart.tests.api");
 
-builder.AddProject<Projects.TurboCart_Presentation_Websites_TurboCartDK>("turbocart.presentation.websites.turbocartdk");
+var authApi = builder.AddProject<Projects.TurboCart_Presentation_Apis_Auth>("AuthenticationAPI");
+var sessionApi = builder.AddProject<Projects.TurboCart_Presentation_Apis_Session>("SessionAPI");
+var turboApi = builder.AddProject<Projects.TurboCart_Presentation_Apis_TurboCart>("TurboCartAPI");
+
+var gatewayApi = builder.AddProject<Projects.TurboCart_Presentation_Apis_Gateway>("GatewayAPI")
+    .WithReference(authApi)
+    .WithReference(turboApi)
+    .WithReference(sessionApi);
+
+builder.AddProject<Projects.TurboCart_Presentation_Websites_TurboCartDK>("TurboCartDK")
+    .WithReference(gatewayApi);
 
 builder.Build().Run();
