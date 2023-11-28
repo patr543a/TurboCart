cb58e707db4ee7cc5284400b6d248c3fb2e2616d
patr543a <113888937+patr543a@users.noreply.github.com>
2023-11-28 09:29:34 +0100

    Disabled warning

--- TurboCart.Infrastructure.Persistance.UnitsOfWork/UnitOfWorkException.cs
+++ TurboCart.Infrastructure.Persistance.UnitsOfWork/UnitOfWorkException.cs
@@ -17,7 +17,9 @@ namespace TurboCart.Infrastructure.Persistance.UnitOfWorks
         {
         }
 
+#pragma warning disable SYSLIB0051 // Type or member is obsolete
         protected UnitOfWorkException(SerializationInfo info, StreamingContext context) : base(info, context)
+#pragma warning restore SYSLIB0051 // Type or member is obsolete
         {
         }
     }
