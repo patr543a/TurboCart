6be023967f118c2a275c5cf2f56cb7a729f6af41
Andrei Lungeanu <andr959m@edu.campusvejle.dk>
2023-11-29 15:02:15 +0100

    Controls for the span of time to show booking for

---
+++ TurboCart.Presentation.Websites.TurboCartManagement/Models/BookingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TurboCart.Presentation.Websites.TurboCartManagement.Models
+{
+    public class BookingViewModel
+    {
+        public int BookingId { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
