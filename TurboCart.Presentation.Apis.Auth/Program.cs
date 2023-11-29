c0c677dbdd9b7f6dc757b5f5fba66fb7b85fd01e
patr543a <113888937+patr543a@users.noreply.github.com>
2023-11-29 10:40:07 +0100

    Implemented auth

--- TurboCart.Presentation.Apis.Auth/Program.cs
+++ TurboCart.Presentation.Apis.Auth/Program.cs
@@ -1,8 +1,25 @@
+using TurboCart.Application.Interfaces;
+using TurboCart.Infrastructure.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
+using TurboCart.Infrastructure.Persistence.Interfaces.Repositories;
+using TurboCart.Infrastructure.Persistance.Repositories;
+using TurboCart.Infrastructure.Persistence.Interfaces.UnitOfWorks;
+using TurboCart.Infrastructure.Persistance.UnitOfWorks;
+using TurboCart.Application.UseCases;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DbContext, TurboCartContext>();
+
+builder.Services.AddTransient<IBookingRepository, BookingRepository>();
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<ITurboCartUnitOfWork, TurboCartUnitOfWork>();
+builder.Services.AddTransient<IUserUseCase, UserUseCase>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
