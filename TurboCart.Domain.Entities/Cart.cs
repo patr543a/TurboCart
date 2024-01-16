a6c0c1c018378465204ca7ed072a74983ab92a91
patr543a <113888937+patr543a@users.noreply.github.com>
2024-01-16 09:15:17 +0100

    Added Cart entity

---
+++ TurboCart.Domain.Entities/Cart.cs
@@ -0,0 +1,11 @@
+﻿namespace TurboCart.Domain.Entities;
+
+public class Cart
+{
+	public int CartNumber { get; set; }
+	public string DriverName { get; set; } = string.Empty;
+	public int LapNumber { get; set; }
+	public TimeSpan LapTime { get; set; }
+	public TimeSpan TotalTime { get; set; }
+	public bool IsFinished { get; set; }
+}
