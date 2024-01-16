8d2101825a9f195eab9716d5c4c5148a7b526934
patr543a <113888937+patr543a@users.noreply.github.com>
2024-01-16 09:17:04 +0100

    Added a base class for a service using SignalR

---
+++ TurboCart.Infrastructure.Networking.Services/SignalRServiceBase.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+
+namespace TurboCart.Infrastructure.Networking.Services;
+
+public abstract class SignalRServiceBase(string url, ILogger logger)
+{
+    private readonly HubConnection _connection = new HubConnectionBuilder()
+        .WithAutomaticReconnect()
+        .WithUrl(url)
+        .Build();
+
+    public HubConnection Connection => _connection;
+
+    protected void Listen<T1>(string methodName, Action<T1> func)
+        => _connection.On(methodName, func);
+
+    protected async Task Invoke(string methodName, object arg1, Action<Exception>? onError = null)
+    {
+        await Connect();
+
+		try
+		{
+			await _connection.InvokeAsync(methodName, arg1);
+		}
+		catch (Exception ex)
+		{
+			logger.LogError(ex, "Failed to invoke");
+
+            onError?.Invoke(ex);
+		}
+	}
+
+    public async Task Connect()
+    {
+        if (_connection.State == HubConnectionState.Connected)
+            return;
+
+        try
+        {
+            await _connection.StartAsync();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to connect");
+        }
+    }
+}
