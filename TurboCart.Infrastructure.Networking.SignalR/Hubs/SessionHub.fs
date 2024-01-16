7c5d873eaf65d2e8c67bcecbd14754b185d671d7
patr543a <113888937+patr543a@users.noreply.github.com>
2024-01-16 09:18:31 +0100

    Added SignalR

---
+++ TurboCart.Infrastructure.Networking.SignalR/Hubs/SessionHub.fs
@@ -0,0 +1,21 @@
+﻿namespace TurboCart.Infrastructure.Networking.SignalR.Hubs
+
+open TurboCart.Infrastructure.Networking.SignalR.Interfaces
+open System.Threading.Tasks
+open TurboCart.Domain.Entities
+open Microsoft.AspNetCore.SignalR
+
+type SessionHub () =
+    inherit Hub<ISessionClient> ()
+
+    static let mutable lastSesion : Option<TurboCartSession> = None
+
+    member this.SessionChanged (session : TurboCartSession) : Task =
+        lastSesion <- Some session
+
+        this.Clients.Others.ReceiveSession session
+
+    override this.OnConnectedAsync () : Task =
+        match lastSesion with
+        | Some s -> this.Clients.Caller.ReceiveSession s
+        | None -> Task.CompletedTask
---
+++ TurboCart.Infrastructure.Networking.SignalR/Interfaces/ISessionClient.fs
@@ -0,0 +1,7 @@
+﻿namespace TurboCart.Infrastructure.Networking.SignalR.Interfaces
+
+open System.Threading.Tasks
+open TurboCart.Domain.Entities
+
+type ISessionClient =
+    abstract member ReceiveSession : TurboCartSession -> Task
