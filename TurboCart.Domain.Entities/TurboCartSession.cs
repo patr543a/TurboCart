39837b610a87892267ded0fcedade4e6326178a8
patr543a <113888937+patr543a@users.noreply.github.com>
2024-01-16 09:15:34 +0100

    Added TurboCartSession entity

---
+++ TurboCart.Domain.Entities/TurboCartSession.cs
@@ -0,0 +1,9 @@
+﻿namespace TurboCart.Domain.Entities;
+
+public class TurboCartSession
+{
+	public bool IsActive { get; set; }
+	public bool IsFinished { get; set; }
+	public TimeSpan TotalTime { get; set; }
+	public IEnumerable<Cart> Carts { get; set; } = [];
+}
